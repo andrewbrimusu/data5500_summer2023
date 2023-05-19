4ad7070fbeb3040889a022a59c93c478e5c34795
Ubuntu <ubuntu@ip-172-31-15-233.us-west-1.compute.internal>
2023-05-19 19:36:47 +0000

    updated code - data5500 summer2023

---
+++ week2_classes_objects/Student.py
@@ -0,0 +1,30 @@
+# encapsulation example, anumber is hidden (private)
+class Student:
+    def __init__(self, anum, name):
+        self.__anum = anum
+        self.name = name
+        
+    def get_anum(self):
+        return self.__anum
+        
+    def set_anum(self, anum):
+        self.__anum = anum
+        
+    def get_name(self):
+        return self.name
+        
+    def set_name(self, name):
+        self.name = name
+        
+        
+andy = Student("a000001", "andy")
+print(andy.get_name())
+
+# print(andy.__anum)
+
+andy.set_anum("a00000001")
+
+print(andy.get_anum())
+
+
+
---
+++ week2_classes_objects/person_class.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+# creating a person with a dictionary
+person = {}
+person["name"] = "andy"
+person["age"] = 39
+person["favorite_colors"] = ["aggie blue", "fighting white"]
+person["hw_scores"] = [95, 80, 99]
+
+print("person: ", person)
+print(person["name"], person["age"], person["favorite_colors"])
+
+# functions with dictionaries are separate
+def calc_avg_grades(grades):
+    return np.mean(grades)
+    
+print(calc_avg_grades(person["hw_scores"]))
+
+
+
+# A class combines data and functions
+class Person():
+    def __init__(self, name, age, favorite_colors, hw_scores=[100, 100, 100]):
+        self.name = name
+        self.age = age
+        self.favorite_colors = favorite_colors
+        self.hw_scores = hw_scores
+        
+    def calc_avg_grades(self):
+        return np.mean(self.hw_scores)
+    
+    def set_name(self, name):
+        self.name = name
+        
+        
+
+
+andy_person = Person("andy", 40, ["aggie blue", "fighting white"], [95, 80, 99])
+ryan = Person("ryan", 21, ["blue"], [100, 99, 100])
+kelley = Person("kelley", 30, ["kelly green"])
+
+
+print(andy_person.calc_avg_grades())
+print(ryan.calc_avg_grades())
+print(ryan.name)
+
+ryan.set_name("ryan sofoul")
+print(ryan.name)
+
